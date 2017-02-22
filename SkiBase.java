88773e5be03904350217dc9c3470c91e48c4deb3
IAlekSashaI <Alexander@MacBook-Pro-Alex.local>
2017-02-22 11:53:53 +0300

    First

---
+++ SkiBase.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Alexander
+ */
+public class SkiBase {
+    String name;
+    String adress;
+    int skiCount;
+    int freeSkiCount;
+    int snowBoardCount;
+    int freeSnowBoardCount;
+    int blackCount;
+    int greenCount;
+    int redCount;
+    int blueCount;
+    int hourPrice;
+    
+    boolean hasLightedSloves;
+    boolean hasTrainers;
+    boolean hasParking;
+    boolean hasCaffe;
+    
+    
+    int mondayPeopleCount;
+    int tuesdayPeopleCount;
+    int wednesdayPeopleCount;
+    int thursdayPeopleCount;
+    int fridayPeopleCount;
+    int satuedayPeopleCount;
+    int sundayPeopleCount;
+    
+    
+    
+}
